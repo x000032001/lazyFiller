8350f7f644be9335ca62a147a993147b133e5b66
num <s000032001@gmail.com>
2015-10-01 22:31:36 +0800

    add css & js

---
+++ html/bar.js
@@ -0,0 +1,68 @@
+document.write('\
+        <!--BEGIN THEME SETTING-->\
+        <div hidden id="theme-setting">\
+            <a href="#" data-toggle="dropdown" data-step="1" data-intro="&lt;b&gt;Many styles&lt;/b&gt; and &lt;b&gt;colors&lt;/b&gt; be created for you. Let choose one and enjoy it!"\
+                data-position="left" class="btn-theme-setting"><i class="fa fa-cog"></i></a>\
+            <div class="content-theme-setting">\
+                <select id="list-style" class="form-control">\
+                    <option value="style1">Flat Squared style</option>\
+                    <option value="style2">Flat Rounded style</option>\
+                    <option value="style3" selected="selected">Flat Border style</option>\
+                </select>\
+            </div>\
+        </div>\
+        <!--END THEME SETTING-->\
+        <!--BEGIN BACK TO TOP-->\
+        <a id="totop" href="#"><i class="fa fa-angle-up"></i></a>\
+        <!--END BACK TO TOP-->\
+        <!--BEGIN TOPBAR-->\
+        <div id="header-topbar-option-demo" class="page-header-topbar">\
+            <nav id="topbar" role="navigation" style="margin-bottom: 0;" data-step="3" class="navbar navbar-default navbar-static-top">\
+            <div class="navbar-header">\
+                <button type="button" data-toggle="collapse" data-target=".sidebar-collapse" class="navbar-toggle"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>\
+                <a id="logo" href="index.html" class="navbar-brand"><span class="fa fa-rocket"></span><span class="logo-text">Your Admin</span><span style="display: none" class="logo-text-icon">µ</span></a></div>\
+            <div class="topbar-main"><a id="menu-toggle" href="#" class="hidden-xs"><i class="fa fa-bars"></i></a>\
+                \
+				<!-- SEARCH BAR\
+                <form id="topbar-search" action="" method="" class="hidden-sm hidden-xs">\
+                    <div class="input-icon right text-white"><a href="#"><i class="fa fa-search"></i></a><input type="text" placeholder="Search here..." class="form-control text-white"/></div>\
+                </form>\
+				-->\
+\
+				<!-- NEWS realtime update\
+                <div class="news-update-box hidden-xs"><span class="text-uppercase mrm pull-left text-white">News:</span>\
+                    <ul id="news-update" class="ticker list-unstyled">\
+                        <li>Welcome to KAdmin - Responsive Multi-Style Admin Template</li>\
+                        <li>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque.</li>\
+                    </ul>\
+                </div>\
+				-->\
+\
+                <ul class="nav navbar navbar-top-links navbar-right mbn">\
+				<!-- unread notification\
+                    <li class="dropdown"><a data-hover="dropdown" href="#" class="dropdown-toggle"><i class="fa fa-bell fa-fw"></i><span class="badge badge-green">3</span></a>\
+                        \
+                    </li>\
+                    <li class="dropdown"><a data-hover="dropdown" href="#" class="dropdown-toggle"><i class="fa fa-envelope fa-fw"></i><span class="badge badge-orange">7</span></a>\
+                        \
+                    </li>\
+                    <li class="dropdown"><a data-hover="dropdown" href="#" class="dropdown-toggle"><i class="fa fa-tasks fa-fw"></i><span class="badge badge-yellow">8</span></a>\
+\
+                    </li>\
+					-->                        \
+\
+                    <li class="dropdown topbar-user"><a data-hover="dropdown" href="#" class="dropdown-toggle"><img src="images/avatar/48.jpg" alt="" class="img-responsive img-circle"/>&nbsp;<span class="hidden-xs">num</span>&nbsp;<span class="caret"></span></a>\
+                        <ul class="dropdown-menu dropdown-user pull-right">\
+                            <li><a href="Login.html"><i class="fa fa-key"></i>Log Out</a></li>\
+                        </ul>\
+                    </li>\
+					<!-- chart\
+                    <li id="topbar-chat" class="hidden-xs"><a href="javascript:void(0)" data-step="4" data-intro="&lt;b&gt;Form chat&lt;/b&gt; keep you connecting with other coworker" data-position="left" class="btn-chat"><i class="fa fa-comments"></i><span class="badge badge-info">3</span></a></li>\
+					-->\
+\
+				</ul> <!-- right side topbar END -->\
+            </div>\
+        </nav>\
+       </div>\
+        <!--END TOPBAR-->\
+');
