05eca215740b18321d849f2a9325c45135b528d6
num <s000032001@gmail.com>
2015-10-01 22:57:56 +0800

    make navbar and sidebar fixed

--- html/bar.js
+++ html/bar.js
@@ -17,7 +17,7 @@ document.write('\
         <!--END BACK TO TOP-->\
         <!--BEGIN TOPBAR-->\
         <div id="header-topbar-option-demo" class="page-header-topbar">\
-            <nav id="topbar" role="navigation" style="margin-bottom: 0;" data-step="3" class="navbar navbar-default navbar-static-top">\
+            <nav id="topbar" role="navigation" style="margin-bottom: 0;" data-step="3" class="navbar navbar-default navbar-fixed-top">\
             <div class="navbar-header">\
                 <button type="button" data-toggle="collapse" data-target=".sidebar-collapse" class="navbar-toggle"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>\
                 <a id="logo" href="index.html" class="navbar-brand"><span class="fa fa-rocket"></span><span class="logo-text">Your Admin</span><span style="display: none" class="logo-text-icon">µ</span></a></div>\
